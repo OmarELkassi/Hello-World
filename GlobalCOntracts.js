b7b4d084cddcc28ce8d14b8b375db2b2764c1ce9
OmarELkassi <44509198+OmarELkassi@users.noreply.github.com>
2018-12-11 22:50:58 +0000

    Create GlobalCOntracts.js

---
+++ GlobalCOntracts.js
@@ -0,0 +1,5 @@
+/*
+COntroller for the GlobalContracts Handling, Intercept Odata Queries
+A HTTP message and interpret / prepare data as expected metadata model
+this Controller inherit from XXXXX and implement ODATA Protocol
+*/
